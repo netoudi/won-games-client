c02d4dc3df1877c989abd44135f19c1e990fae32
netoudi <netoudi@rocketmail.com>
2021-01-27 11:01:19 -0300

    Add option to size on logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -27,4 +27,20 @@ describe('<Logo />', () => {
       color: '#030517',
     })
   })
+
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
+
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+      width: '11rem',
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+      width: '20rem',
+    })
+  })
 })
