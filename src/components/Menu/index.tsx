769bc974b761edad9140cb66271f4d9843c8391a
netoudi <netoudi@rocketmail.com>
2021-01-28 17:00:57 -0300

    Add styles to MenuFull

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,6 +4,7 @@ import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outline
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
+import Button from 'components/Button'
 import Logo from 'components/Logo'
 import * as S from './styles'
 
@@ -32,6 +33,21 @@ const Menu = () => {
 
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explorer</S.MenuLink>
+        </S.MenuNav>
+
+        <S.RegisterBox>
+          <Button fullWidth size="large">
+            Login in now
+          </Button>
+          <span>or</span>
+          <S.CreateAccount href="#" title="Sign up">
+            Sign up
+          </S.CreateAccount>
+        </S.RegisterBox>
       </S.MenuFull>
     </S.Wrapper>
   )
