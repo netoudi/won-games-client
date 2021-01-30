bf38cf7a81f868f6077006fecb54ed1f5e18a17a
netoudi <netoudi@rocketmail.com>
2021-01-30 16:00:00 -0300

    Add highlight floatImage

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,5 +1,6 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Highlight, { HighlightProps } from '.'
+import Banner from 'components/Banner'
 
 export default {
   title: 'Highlight',
@@ -13,4 +14,14 @@ export default {
   },
 } as Meta
 
-export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
+export const Default: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const WithFloatImage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Highlight {...args} floatImage="/img/red-dead-float.png" />
+  </div>
+)
