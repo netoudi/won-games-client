cfba5d856082ab44d71b45221af9841890c2399b
netoudi <netoudi@rocketmail.com>
2021-01-30 15:07:00 -0300

    Add highlight initial structure

---
+++ src/components/Highlight/test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react'
+import Highlight from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+const props = {
+  title: 'Heading 1',
+  subtitle: 'Heading 2',
+  buttonLabel: 'Buy now',
+  buttonLink: '/rdr2',
+}
+
+describe('<Highlight />', () => {
+  it('should render headings and button', () => {
+    renderWithTheme(<Highlight {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /heading 1/i }),
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /heading 2/i }),
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toBeInTheDocument()
+  })
+})
