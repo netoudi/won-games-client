a9c3935cf6a04931b4a32d8af0c5f016ecbf3f69
netoudi <netoudi@rocketmail.com>
2021-02-02 16:47:47 -0300

    Connect mock data to the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,24 @@
-import Home from 'templates/Home'
+import Home, { HomeTemplateProps } from 'templates/Home'
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
-export default function Index(props: any) {
+export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-// ATENÇÃO:
-// os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
-
-// getStaticProps => gerar estático em build time
-// getServerSideProps => gerar via ssr a cada request
-// getInitialProps => gerar via ssr a cada request
 export function getServerSideProps() {
-  // faz lógica
-  // pode ser buscar dados numa API
-  // fazer calculo|leitura de context
-
-  // retorno dos dados
   return {
     props: {
-      heading: 'Hello world!',
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcomingGames: gamesMock,
+      upcomingHighlight: highlightMock,
+      upcomingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighlight: highlightMock,
     },
   }
 }
