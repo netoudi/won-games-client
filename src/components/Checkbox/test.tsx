8fb62a9f411743e87e2bfc51333906c0bb5c61d5
netoudi <netoudi@rocketmail.com>
2021-02-03 09:06:04 -0300

    Create Checkbox initial structure - pure input/label

---
+++ src/components/Checkbox/test.tsx
@@ -0,0 +1,9 @@
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Checkbox from '.'
+
+describe('<Checkbox />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(<Checkbox />)
+  })
+})
