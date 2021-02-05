934f45167ac983fb9fa7239bb9e8309c84b6c613
netoudi <netoudi@rocketmail.com>
2021-02-05 17:10:57 -0300

    Add generic Form styled component

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -1,12 +1,11 @@
 import Link from 'next/link'
 import { AccountCircle, Email, Lock } from '@styled-icons/material-outlined'
+import { FormLink, FormWrapper } from 'components/Form'
 import TextField from 'components/TextField'
 import Button from 'components/Button'
 
-import * as S from './styles'
-
 const FormSignUp = () => (
-  <S.Wrapper>
+  <FormWrapper>
     <form>
       <TextField
         name="name"
@@ -40,14 +39,14 @@ const FormSignUp = () => (
         Sign up now
       </Button>
 
-      <S.FormLink>
+      <FormLink>
         Already have an account?{' '}
         <Link href="/sign-in">
           <a>Sign in</a>
         </Link>
-      </S.FormLink>
+      </FormLink>
     </form>
-  </S.Wrapper>
+  </FormWrapper>
 )
 
 export default FormSignUp
