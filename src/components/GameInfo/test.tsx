76cd9c6423daacf791ecf9f9953e8a14dfa38e38
netoudi <netoudi@rocketmail.com>
2021-02-09 16:20:53 -0300

    Add html and tests to GameInfo

--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -6,7 +6,7 @@ import GameInfo from '.'
 const props = {
   title: 'My Game Title',
   description: 'Game Description',
-  price: '210.00',
+  price: '210,00',
 }
 
 describe('<GameInfo />', () => {
@@ -14,14 +14,28 @@ describe('<GameInfo />', () => {
     renderWithTheme(<GameInfo {...props} />)
 
     // esperar por um heading (title)
+    expect(
+      screen.getByRole('heading', { name: /my game title/i }),
+    ).toBeInTheDocument()
+
     // esperar por description
+    expect(screen.getByText(/game description/i)).toBeInTheDocument()
+
     // esperar pelo price
+    expect(screen.getByText(/\$210,00/i)).toBeInTheDocument()
   })
 
   it('should render buttons', () => {
     renderWithTheme(<GameInfo {...props} />)
 
     // esperar button add to cart
+    expect(
+      screen.getByRole('button', { name: /add to cart/i }),
+    ).toBeInTheDocument()
+
     // esperar button wishlist
+    expect(
+      screen.getByRole('button', { name: /wishlist/i }),
+    ).toBeInTheDocument()
   })
 })
