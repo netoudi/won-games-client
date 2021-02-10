f6a70c80fbe6e994e1eafdf32d98712d9b7cfbd2
netoudi <netoudi@rocketmail.com>
2021-02-10 10:00:30 -0300

    Add developer and releaseDate to GameDetails

--- src/components/GameDetails/test.tsx
+++ src/components/GameDetails/test.tsx
@@ -1,14 +1,50 @@
-import { render, screen } from '@testing-library/react'
-import GameDetails from '.'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameDetails, { GameDetailsProps } from '.'
+
+const props: GameDetailsProps = {
+  developer: 'Different Tales',
+  platforms: ['windows', 'linux', 'mac'],
+  releaseDate: '2020-11-21T23:00:00',
+}
 
 describe('<GameDetails />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameDetails />)
+  it('should render the blocks', () => {
+    renderWithTheme(<GameDetails {...props} />)
 
     expect(
-      screen.getByRole('heading', { name: /GameDetails/i }),
+      screen.getByRole('heading', { name: /developer/i }),
     ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: /release date/i }),
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /platforms/i }),
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /publisher/i }),
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /rating/i })).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /genres/i })).toBeInTheDocument()
+  })
+
+  it('should render platform icons', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByRole('img', { name: /windows/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /linux/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /mac/i })).toBeInTheDocument()
+  })
+
+  it('should render the formated date', function () {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText('Nov 21, 2020')).toBeInTheDocument()
   })
 })
