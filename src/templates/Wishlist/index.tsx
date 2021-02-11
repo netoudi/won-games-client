1062b4676dbb02a0e62c5919f6c70efa1dbc9d8c
netoudi <netoudi@rocketmail.com>
2021-02-11 12:07:16 -0300

    Add Empty to Wishlist

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -6,6 +6,7 @@ import { Grid } from 'components/Grid'
 import GameCard, { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
 import { Divider } from 'components/Divider'
+import Empty from 'components/Empty'
 
 export type WishlistTemplateProps = {
   games?: GameCardProps[]
@@ -14,7 +15,7 @@ export type WishlistTemplateProps = {
 }
 
 const Wishlist = ({
-  games,
+  games = [],
   recommendedGames,
   recommendedHighlight,
 }: WishlistTemplateProps) => (
@@ -24,11 +25,19 @@ const Wishlist = ({
         Wishlist
       </Heading>
 
-      <Grid>
-        {games?.map((game, index) => (
-          <GameCard key={`wishlist-${index}`} {...game} />
-        ))}
-      </Grid>
+      {games.length ? (
+        <Grid>
+          {games?.map((game, index) => (
+            <GameCard key={`wishlist-${index}`} {...game} />
+          ))}
+        </Grid>
+      ) : (
+        <Empty
+          title="Your wishlist is empty"
+          description="Games added to your wishlist will appear here"
+          hasLink
+        />
+      )}
 
       <Divider />
     </Container>
--- src/templates/Wishlist/test.tsx
+++ src/templates/Wishlist/test.tsx
@@ -1,14 +1,14 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
-import gemesMock from 'components/GameCardSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
 import Wishlist from '.'
 
 const props = {
-  games: gemesMock,
-  recommendedGames: gemesMock,
+  games: gamesMock,
+  recommendedGames: gamesMock,
   recommendedHighlight: highlightMock,
 }
 
@@ -49,5 +49,20 @@ describe('<Wishlist />', () => {
 
     expect(screen.getAllByText(/population zero/i)).toHaveLength(6)
     expect(screen.getByTestId(/mock showcase/i)).toBeInTheDocument()
+  })
+
+  it('should render empty when there are no games', () => {
+    renderWithTheme(
+      <Wishlist
+        recommendedGames={gamesMock}
+        recommendedHighlight={highlightMock}
+      />,
+    )
+
+    expect(screen.queryByText(/population zero/i)).not.toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /your wishlist is empty/i }),
+    ).toBeInTheDocument()
   })
 })
