c1067d3daea0c061cecbe56e640a8e580127ac49
netoudi <netoudi@rocketmail.com>
2021-02-11 09:02:18 -0300

    Add tests to Game page

--- src/templates/Game/test.tsx
+++ src/templates/Game/test.tsx
@@ -1,9 +1,133 @@
+import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
-import Game from '.'
+import { GameDetailsProps } from 'components/GameDetails'
+import galleryMock from 'components/Gallery/mock'
+import gemeInfoMock from 'components/GameInfo/mock'
+import gemesMock from 'components/GameCardSlider/mock'
+import gameDetailsMock from 'components/GameDetails/mock'
+import highlightMock from 'components/Highlight/mock'
+
+import Game, { GameTemplateProps } from '.'
+
+const props: GameTemplateProps = {
+  cover: 'bg-image.jpg',
+  gameInfo: gemeInfoMock,
+  gallery: galleryMock,
+  description: `<h1>Custom HTML</h1>`,
+  details: gameDetailsMock as GameDetailsProps,
+  upcomingGames: gemesMock,
+  upcomingHighlight: highlightMock,
+  recommendGames: gemesMock,
+}
+
+jest.mock('components/Menu', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Menu" />
+    },
+  }
+})
+
+jest.mock('components/Footer', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Footer" />
+    },
+  }
+})
+
+jest.mock('components/GameInfo', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock GameInfo" />
+    },
+  }
+})
+
+jest.mock('components/Gallery', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Gallery" />
+    },
+  }
+})
+
+jest.mock('components/GameDetails', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock GameDetails" />
+    },
+  }
+})
+
+jest.mock('components/Showcase', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Showcase" />
+    },
+  }
+})
 
 describe('<Game />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Game />)
+  it('should render the template with component', () => {
+    renderWithTheme(<Game {...props} />)
+
+    expect(screen.getByTestId('Mock GameInfo')).toBeInTheDocument()
+    expect(screen.getByTestId('Mock Gallery')).toBeInTheDocument()
+    expect(screen.getByTestId('Mock GameDetails')).toBeInTheDocument()
+    expect(screen.getAllByTestId('Mock Showcase')).toHaveLength(2)
+    expect(screen.getByText(/custom html/i)).toBeInTheDocument()
+  })
+
+  it('should not render the gallery if no images', function () {
+    renderWithTheme(<Game {...props} gallery={undefined} />)
+
+    expect(screen.queryByTestId('Mock Gallery')).not.toBeInTheDocument()
+  })
+
+  it('should not render the gallery on mobile', function () {
+    renderWithTheme(<Game {...props} />)
+
+    expect(screen.getByTestId('Mock Gallery').parentElement).toHaveStyle({
+      display: 'none',
+    })
+
+    expect(screen.getByTestId('Mock Gallery').parentElement).toHaveStyleRule(
+      'display',
+      'block',
+      {
+        media: '(min-width: 768px)',
+      },
+    )
+  })
+
+  it('should render the cover image', function () {
+    renderWithTheme(<Game {...props} />)
+
+    const cover = screen.getByRole('image', { name: /cover/i })
+
+    expect(cover).toHaveStyle({
+      backgroundImage: 'url(bg-image.jpg)',
+      height: '39.5rem',
+    })
+
+    expect(cover).toHaveStyleRule('height', '70rem', {
+      media: '(min-width: 768px)',
+    })
+
+    expect(cover).toHaveStyleRule(
+      'clip-path',
+      'polygon(0 0,100% 0,100% 100%,0 85%)',
+      {
+        media: '(min-width: 768px)',
+      },
+    )
   })
 })
