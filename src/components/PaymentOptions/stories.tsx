49a7658d8e5b263e32f5711816f2633b7388cf35
netoudi <netoudi@rocketmail.com>
2021-02-15 13:57:57 -0300

    Add PaymentOptions structure

---
+++ src/components/PaymentOptions/stories.tsx
@@ -0,0 +1,16 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import cardsMock from './mock'
+
+import PaymentOptions, { PaymentOptionsProps } from '.'
+
+export default {
+  title: 'PaymentOptions',
+  component: PaymentOptions,
+  args: {
+    cards: cardsMock,
+  },
+} as Meta
+
+export const Default: Story<PaymentOptionsProps> = (args) => (
+  <PaymentOptions {...args} />
+)
