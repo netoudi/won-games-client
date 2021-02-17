f8e7ff621dbaec57fc88eebcf48eab660ecbb800
netoudi <netoudi@rocketmail.com>
2021-02-17 09:42:15 -0300

    Add OrdersList

---
+++ src/components/OrdersList/test.tsx
@@ -0,0 +1,40 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import OrdersList from '.'
+import itemsMock from './mock'
+
+jest.mock('components/Empty', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Empty" />
+    },
+  }
+})
+
+jest.mock('components/GameItem', () => {
+  return {
+    __esModule: true,
+    default: function Mock({ children }: { children: React.ReactNode }) {
+      return <div data-testid="Mock GameItem">{children}</div>
+    },
+  }
+})
+
+describe('<OrdersList />', () => {
+  it('should render the game items', () => {
+    renderWithTheme(<OrdersList items={itemsMock} />)
+
+    expect(
+      screen.getByRole('heading', { name: /my orders/i }),
+    ).toBeInTheDocument()
+
+    expect(screen.getAllByTestId('Mock GameItem')).toHaveLength(2)
+  })
+
+  it('should render empty state', () => {
+    renderWithTheme(<OrdersList items={[]} />)
+
+    expect(screen.getByTestId('Mock Empty')).toBeInTheDocument()
+  })
+})
