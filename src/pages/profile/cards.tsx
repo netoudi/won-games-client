8e530a6f9c6581f6534d3193ec5cfa7904d91b9e
netoudi <netoudi@rocketmail.com>
2021-02-17 09:12:15 -0300

    Add /profile/cards page

---
+++ src/pages/profile/cards.tsx
@@ -0,0 +1,19 @@
+import Profile from 'templates/Profile'
+import CardsList, { CardsListProps } from 'components/CardsList'
+import cardsMock from 'components/PaymentOptions/mock'
+
+export default function ProfileCards({ cards }: CardsListProps) {
+  return (
+    <Profile>
+      <CardsList cards={cards} />
+    </Profile>
+  )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      cards: cardsMock,
+    },
+  }
+}
