1d96d02357a4d697e148186c4419707f28ea7cb9
netoudi <netoudi@rocketmail.com>
2021-02-19 10:00:00 -0300

    Add CartDropdown

---
+++ src/components/CartDropdown/stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import CartDropdown, { CartDropdownProps } from '.'
+import itemsMock from 'components/CartList/mock'
+
+export default {
+  title: 'CartDropdown',
+  component: CartDropdown,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark',
+    },
+  },
+  args: {
+    items: itemsMock,
+    total: 'R$ 300,00',
+  },
+} as Meta
+
+export const Default: Story<CartDropdownProps> = (args) => (
+  <div style={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
+    <CartDropdown {...args} />
+  </div>
+)
---
+++ src/components/CartDropdown/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+import * as CartListStyles from 'components/CartList/styles'
+
+export const Wrapper = styled.main`
+  ${CartListStyles.Wrapper} {
+    width: 56rem;
+  }
+`
