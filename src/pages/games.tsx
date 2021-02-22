7f74fd6ba26e602d4775fc3a13e9fe43ae5e27db
netoudi <netoudi@rocketmail.com>
2021-02-22 15:21:31 -0300

    Create ApolloClient directly to home

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,8 +1,24 @@
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
 import Games, { GamesTemplateProps } from 'templates/Games'
 import filterItemsMock from 'components/ExploreSidebar/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 
 export default function GamesPage(props: GamesTemplateProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache(),
+  })
+
+  client.query({
+    query: gql`
+      query GetGames {
+        games {
+          name
+        }
+      }
+    `,
+  })
+
   return <Games {...props} />
 }
 
