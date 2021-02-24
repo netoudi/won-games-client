ab980a6722d6143f2fb196509d15b16144119c4c
netoudi <netoudi@rocketmail.com>
2021-02-24 14:44:57 -0300

    Refactor to use mappers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
 import Home, { HomeTemplateProps } from 'templates/Home'
-import gamesMock from 'components/GameCardSlider/mock'
-import highlightMock from 'components/Highlight/mock'
 import { initializeApollo } from 'utils/apollo'
 import { QueryHome } from 'graphql/generated/QueryHome'
 import { QUERY_HOME } from 'graphql/queries/home'
+import { bannersMapper, gamesMapper, highlightMapper } from 'utils/mappers'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
@@ -17,53 +16,27 @@ export async function getStaticProps() {
   return {
     props: {
       revalidate: 60,
-      banners: data.banners.map((banner) => ({
-        img: `http://localhost:1337${banner.image?.url}`,
-        title: banner.title,
-        subtitle: banner.subtitle,
-        buttonLabel: banner.button?.label,
-        buttonLink: banner.button?.link,
-        ...(banner.ribbon && {
-          ribbon: banner.ribbon.text,
-          ribbonColor: banner.ribbon.color,
-          ribbonSize: banner.ribbon.size,
-        }),
-      })),
+
+      banners: bannersMapper(data.banners),
+
       newGamesTitle: data.sections?.newGames?.title,
-      newGames: data.newGames.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
-        price: game.price,
-      })),
+      newGames: gamesMapper(data.newGames),
+
       mostPopularGamesTitle: data.sections?.popularGames?.title,
-      mostPopularHighlight: highlightMock,
-      mostPopularGames: data.sections!.popularGames!.games.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
-        price: game.price,
-      })),
+      mostPopularHighlight: highlightMapper(
+        data.sections?.popularGames?.highlight,
+      ),
+      mostPopularGames: gamesMapper(data.sections?.popularGames?.games),
+
       upcomingGamesTitle: data.sections?.upcomingGames?.title,
-      upcomingGames: data.upcomingGames.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
-        price: game.price,
-      })),
-      upcomingHighlight: highlightMock,
+      upcomingHighlight: highlightMapper(
+        data.sections?.upcomingGames?.highlight,
+      ),
+      upcomingGames: gamesMapper(data.upcomingGames),
+
       freeGamesTitle: data.sections?.freeGames?.title,
-      freeGames: data.freeGames.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
-        price: game.price,
-      })),
-      freeHighlight: highlightMock,
+      freeHighlight: highlightMapper(data.sections?.freeGames?.highlight),
+      freeGames: gamesMapper(data.freeGames),
     },
   }
 }
