619347534ec2da9342421eeff2728a53b77c146c
netoudi <netoudi@rocketmail.com>
2021-02-25 15:25:57 -0300

    Add useQuery to Games Page

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,17 +1,24 @@
+import { useQuery } from '@apollo/client'
 import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown'
 import Base from 'templates/Base'
 import { Grid } from 'components/Grid'
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
-import GameCard, { GameCardProps } from 'components/GameCard'
+import GameCard from 'components/GameCard'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
+import { QUERY_GAMES } from 'graphql/queries/games'
 
 import * as S from './styles'
 
 export type GamesTemplateProps = {
   filterItems: ItemProps[]
-  games?: GameCardProps[]
 }
 
-const Games = ({ filterItems, games }: GamesTemplateProps) => {
+const Games = ({ filterItems }: GamesTemplateProps) => {
+  const { data, loading } = useQuery<QueryGames, QueryGamesVariables>(
+    QUERY_GAMES,
+    { variables: { limit: 15 } },
+  )
+
   const handleFilter = () => {
     return
   }
@@ -27,8 +34,15 @@ const Games = ({ filterItems, games }: GamesTemplateProps) => {
 
         <section>
           <Grid>
-            {games?.map((game) => (
-              <GameCard key={game.title} {...game} />
+            {data?.games.map((game) => (
+              <GameCard
+                key={game.slug}
+                title={game.name}
+                slug={game.slug}
+                developer={game.developers[0].name}
+                img={`http://localhost:1337${game.cover?.url}`}
+                price={game.price}
+              />
             ))}
           </Grid>
 
