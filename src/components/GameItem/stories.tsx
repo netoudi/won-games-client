57bc6a2d1161f6bf712603af44e89f47c90c5944
netoudi <netoudi@rocketmail.com>
2021-03-05 12:07:57 -0300

    Add remove to GameItem and fix pages/game to pass down the id

--- src/components/GameItem/stories.tsx
+++ src/components/GameItem/stories.tsx
@@ -5,6 +5,7 @@ export default {
   title: 'GameItem',
   component: GameItem,
   args: {
+    id: '1',
     img: 'https://source.unsplash.com/user/willianjusten/151x70',
     title: 'Red Dead Redemption 2',
     price: 'R$ 215,00',
