fdc8b09791664dd0054e964c045346842ec53948
netoudi <netoudi@rocketmail.com>
2021-03-15 14:58:57 -0300

    Removing ugly autofill

--- src/components/TextField/styles.ts
+++ src/components/TextField/styles.ts
@@ -31,7 +31,14 @@ export const Input = styled.input<IconPositionProps>`
     background: transparent;
     border: 0;
     outline: none;
-    width: 100%;
+    width: ${iconPosition === 'right' ? `calc(100% - 2.2rem)` : `100%`};
+
+    &:-webkit-autofill {
+      -webkit-box-shadow: 0 0 0 ${theme.spacings.small} ${
+    theme.colors.lightGray
+  } inset;
+      filter: none;
+    }
   `}
 `
 
@@ -68,6 +75,7 @@ const wrapperModifiers = {
     ${InputWrapper} {
       border-color: ${theme.colors.red};
     }
+
     ${Icon},
     ${Label} {
       color: ${theme.colors.red};
