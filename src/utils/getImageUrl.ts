2e021e39114d4fb161c511145a3f06789827fd20
netoudi <netoudi@rocketmail.com>
2021-04-01 13:45:00 -0300

    Fix getImageUrl

--- src/utils/getImageUrl.ts
+++ src/utils/getImageUrl.ts
@@ -1,7 +1,6 @@
-export default function getImageUrl(url: string | undefined | null) {
-  const baseUrl = process.env.NEXT_PUBLIC_IMAGE_HOST || 'http://localhost:1337'
-  const noImage =
-    process.env.NEXT_PUBLIC_NO_IMAGE_URL || baseUrl + '/uploads/no_image.png'
+export default function getImageUrl(url?: string | null) {
+  const baseUrl = process.env.NEXT_PUBLIC_IMAGE_HOST
+  const noImage = process.env.NEXT_PUBLIC_NO_IMAGE_URL
 
   return url ? baseUrl + url : noImage
 }
