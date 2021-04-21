6c42067401218f866b9c4d73fb667a998364a4f1
netoudi <netoudi@rocketmail.com>
2021-04-21 11:03:27 -0300

    Add WishlistButton to GameInfo

--- src/components/GameInfo/index.tsx
+++ src/components/GameInfo/index.tsx
@@ -1,9 +1,7 @@
-import { FavoriteBorder } from '@styled-icons/material-outlined'
-
-import Button from 'components/Button'
 import Heading from 'components/Heading'
 import Ribbon from 'components/Ribbon'
 import CartButton from 'components/CartButton'
+import WishlistButton from 'components/WishlistButton'
 import formatPrice from 'utils/format-price'
 
 import * as S from './styles'
@@ -27,10 +25,7 @@ const GameInfo = ({ id, title, description, price }: GameInfoProps) => (
 
     <S.ButtonsWrapper>
       <CartButton id={id} size="large" hasText />
-
-      <Button icon={<FavoriteBorder />} size="large" minimal>
-        Wishlist
-      </Button>
+      <WishlistButton id={id} size="large" hasText />
     </S.ButtonsWrapper>
   </S.Wrapper>
 )
