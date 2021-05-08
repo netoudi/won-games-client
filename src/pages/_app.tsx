f73c6ee1aea59c322cfab40cf837d5818d3fca5a
netoudi <netoudi@rocketmail.com>
2021-05-08 17:13:00 -0300

    Configure manifest to PWA and icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,9 +28,10 @@ function App({ Component, pageProps }: AppProps) {
                 <link rel="shortcut icon" href="/img/icon-512.png" />
                 <link rel="apple-touch-icon" href="/img/icon-512.png" />
                 <link rel="manifest" href="/manifest.json" />
+                <meta name="theme-color" content="#06092B" />
                 <meta
                   name="description"
-                  content="The best Game Stores in the world!"
+                  content="The best Game Store in the world!"
                 />
               </Head>
               <DefaultSeo {...SEO} />
