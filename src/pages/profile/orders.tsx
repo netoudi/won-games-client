5dd0481c04ad27277e22fc79d1aee9817fd69e3a
netoudi <netoudi@rocketmail.com>
2021-05-08 09:16:00 -0300

    Prevent to get cache from orders page

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -27,6 +27,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     variables: {
       identifier: session?.id,
     },
+    fetchPolicy: 'no-cache',
   })
 
   return {
